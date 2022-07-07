a957e4372a98258247036db3e27ab444ed294ccf
Ian Iversen <ianiversen1@gmail.com>
2022-07-07 13:00:44 -0400

    commit tree 09cbec86ecbe13bc96c3b27b07359fd06f994459

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json))
+  renderBooks(json)
   
 }
 
@@ -15,3 +18,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
